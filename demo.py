4ef11ef7861eecd4802e84efdaeaa4353593dfc1
91630 <kalyankanna576@gmail.com>
2022-04-29 18:09:07 +0530

    few klines DDED

--- demo.py
+++ demo.py
@@ -9,3 +9,10 @@
         String baseUrl = "http://demo.guru99.com/test/newtours/";
         String expectedTitle = "Welcome: Mercury Tours";
         String actualTitle = "";
+from selenium import weddriver
+ new FirefoxDriver();
+		//comment the above 2 lines and uncomment below 2 lines to use Chrome
+		//System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();
+
+        String baseUrl = "http://dsemo.guru99.com/test/newtu
